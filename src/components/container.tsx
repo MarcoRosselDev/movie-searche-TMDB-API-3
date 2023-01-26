4e0cb514196446235b3bba217ee00ec422b8bfc9
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-01-26 19:02:40 -0300

    Header structure 1

--- src/components/container.tsx
+++ src/components/container.tsx
@@ -3,7 +3,7 @@ import { mergeClassName } from '../utils'
 
 export const Container = (props: CustomComponentProps) => {
   return (
-    <div className={mergeClassName('px-6 py-1.5 max-w-screen-lg mx-auto')}>
+    <div className={mergeClassName('px-6 py-3 max-w-screen-lg mx-auto')}>
       {props.children}
     </div>
   )
--- src/leyouts/Header.tsx
+++ src/leyouts/Header.tsx
@@ -1,15 +1,35 @@
-import { Link } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import { Link, useLocation } from 'react-router-dom'
 import { Container } from '../components/container'
 
+const MENU_CLASS = `
+  px-3
+  py-1.5
+  hover:bg-primary
+`
+
+const MENU_CLASS_ACTIVE = `
+  bg-primary
+`
+
 export const Header = () => {
+  const location = useLocation()
+  const [pathname, setPathname] = useState()
+
+  useEffect(() => {}, [location.pathname])
+
   return (
     <div className="bg-header">
       <Container className="flex justify-between">
         {/* brand and menu */}
         <div className="flex items-center gap-6">
-          <h1>
+          <h1 className="text-2xl">
             <Link to={'/'}>My Movie App</Link>
           </h1>
+          <div className="flex items-center gap-1.5">
+            <Link to={'/movies'}>Movies</Link>
+            <Link to={'/tv'}>TV</Link>
+          </div>
         </div>
       </Container>
     </div>
