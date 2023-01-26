c4a8aa797977efe27bf274e7e38427a1f18de066
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-01-25 23:17:02 -0300

    Header tsx structure

--- src/components/container.tsx
+++ src/components/container.tsx
@@ -2,5 +2,9 @@ import { CustomComponentProps } from '../interfaces'
 import { mergeClassName } from '../utils'
 
 export const Container = (props: CustomComponentProps) => {
-  return <div className={mergeClassName('')}></div>
+  return (
+    <div className={mergeClassName('px-6 py-1.5 max-w-screen-lg mx-auto')}>
+      {props.children}
+    </div>
+  )
 }
--- src/leyouts/Header.tsx
+++ src/leyouts/Header.tsx
@@ -1,9 +1,17 @@
+import { Link } from 'react-router-dom'
 import { Container } from '../components/container'
 
 export const Header = () => {
   return (
-    <div>
-      <Container />
+    <div className="bg-header">
+      <Container className="flex justify-between">
+        {/* brand and menu */}
+        <div className="flex items-center gap-6">
+          <h1>
+            <Link to={'/'}>My Movie App</Link>
+          </h1>
+        </div>
+      </Container>
     </div>
   )
 }
