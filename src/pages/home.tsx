2d5d6cb56ce57f17c96f1ae8ae76b14b8fe979e1
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-01-27 23:40:19 -0300

    home fetchTrending function

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,11 +1,32 @@
 import { Section } from '../components/section'
 import Slider from 'react-slick'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Film } from '../interfaces'
 
 export const Home = () => {
   //
   const [trendings, setTrendings] = useState<Film[]>([])
+
+  const fetchTrending = () => {
+    const arrs: Film[] = []
+    for (let i = 0; i < 5; i++) {
+      arrs.push({
+        id: i,
+        title: 'Lorem ipsumdolo sit emet consectetur adipsisicing elit,',
+        description:
+          'Lorem ipsumdolo sit emet consectetur adipsisicing elit, quam, susciipit? pariatur non ipse alias at, iure, repellat',
+        coverPath: '',
+        genreIds: [1, 2, 3, 4, 5, 6],
+        posterPath: '',
+        seasons: [],
+      })
+    }
+    setTrendings(arrs)
+  }
+
+  useEffect(() => {
+    fetchTrending()
+  }, [])
 
   return (
     <>
