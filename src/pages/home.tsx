b91ef452db62d998b228121e391f78d3757a9135
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-01-27 23:35:45 -0300

    home section slider

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,18 @@
+import { Section } from '../components/section'
+import Slider from 'react-slick'
+import { useState } from 'react'
+import { Film } from '../interfaces'
+
 export const Home = () => {
+  //
+  const [trendings, setTrendings] = useState<Film[]>([])
+
   return (
     <>
       {/* trendings */}
+      <Section>
+        <Slider></Slider>
+      </Section>
       {/* in theaters */}
       {/* populars */}
       {/* top rated tv */}
