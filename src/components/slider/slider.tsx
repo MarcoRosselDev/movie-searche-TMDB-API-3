78a0ac51bd5bbb34ce091bead12da71f9efd674a
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-01-29 03:30:01 -0300

    slider if

--- src/components/slider/slider.tsx
+++ src/components/slider/slider.tsx
@@ -2,9 +2,44 @@ import './slider.css'
 
 import Slick, { Settings } from 'react-slick'
 
-export const Slider = (props: Settings) => {
+interface Props extends Settings {
+  isMovieCard?: boolean
+}
+
+export const Slider = (props: Props) => {
+  //
+  let settings: Settings = {
+    ...props,
+  }
+
+  if (props.isMovieCard) {
+    settings = {
+      ...settings,
+      infinite: true,
+      swipe: false,
+      slidesToShow: 5,
+      slidesToScroll: 5,
+      responsive: [
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+          },
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+          },
+        },
+      ],
+    }
+  }
+
   return (
-    <Slick autoplay={false} {...props} autoplaySpeed={5000}>
+    <Slick autoplay={false} {...settings} autoplaySpeed={5000}>
       {props.children}
     </Slick>
   )
