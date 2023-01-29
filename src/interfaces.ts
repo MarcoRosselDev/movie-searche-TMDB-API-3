eade1cc0b4c0d5b2440b482e1e2f2c5ee60ca132
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-01-29 15:48:47 -0300

    interfeces exported

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,4 +1,5 @@
 import { ReactNode } from 'react'
+
 import { MediaType } from './types'
 
 export interface CustomComponentProps {
@@ -6,9 +7,25 @@ export interface CustomComponentProps {
   className?: string
 }
 
+export interface Episode {
+  id: number
+  title: string
+  overview: string
+  airDate: string
+  stillPath: string
+  episodeNumber: number
+}
+
 export interface Season {
   id: number
+  filmName: string
+  name: string
+  seasonNumber: number
+  posterPath: string
+  episodes: Episode[]
+  airDate: string
 }
+
 export interface Film {
   id: number
   mediaType: MediaType
@@ -18,4 +35,21 @@ export interface Film {
   coverPath: string
   genreIds: number[]
   seasons: Season[]
+}
+
+export interface Cast {
+  id: number
+  name: string
+  characterName: string
+  profilePath: string
+}
+
+export interface Trailer {
+  id: number
+  key: string
+}
+
+export interface Genre {
+  id: number
+  name: string
 }
