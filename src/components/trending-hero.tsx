f20f6e48603cdca5da2805bb808d8e659c8535a6
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-01-28 23:56:16 -0300

    trending-hero fixed

--- src/components/trending-hero.tsx
+++ src/components/trending-hero.tsx
@@ -8,18 +8,19 @@ interface Props {
 
 export const TrendingsHero = (props: Props) => {
   return (
-    <div className="h-[300px] relative flex justify-center">
+    <div className="h-[300px] relative flex items-center cursor-pointer">
       {/* bg image */}
       <div className="absolute left-0 top-0 right-0 bottom-0">
+        <div className="overlay-slick-hero"></div>
         <Image src=""></Image>
       </div>
       {/* text */}
-      <div className="flex flex-col items-start">
-        <p className="text-xl mx-w-[50%] truncate">{props.film.title}</p>
+      <div className="flex flex-col gap-3 items-start relative z-10 mx-[55px] max-w-[50%] mobile:max-w-[100%]">
+        <p className="text-xl line-clamp-1">{props.film.title}</p>
         <p className="text-sm line-clamp-3">{props.film.description}</p>
-        <button className="px-3 py-1.5 flex items-center gap-3">
+        <button className="px-3 py-1.5 flex items-center gap-3 bg-primary rounded-md">
           <MdPlayCircleOutline size={18}></MdPlayCircleOutline>
-          <span>Play trailers</span>
+          <span>Play trailer</span>
         </button>
       </div>
     </div>
