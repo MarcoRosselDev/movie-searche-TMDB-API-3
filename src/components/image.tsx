6da39c95c1697bc4880364df1745ecf3928ee202
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-01-28 23:48:41 -0300

    image fixed

--- src/components/image.tsx
+++ src/components/image.tsx
@@ -8,9 +8,15 @@ interface Props extends CustomComponentProps {
 export const Image = (props: Props) => {
   return (
     <div
-      className={mergeClassName('bg-primary h-full w-full', props.className)}
+      className={mergeClassName(
+        'bg-primary h-full w-full rounded-lg overflow-hidden',
+        props.className
+      )}
     >
-      <img src={props.src} className="w-full h-full" alt="image ts"></img>
+      <img
+        src={props.src}
+        className="min-h-[200px] w-full h-full object-cover"
+      ></img>
     </div>
   )
 }
