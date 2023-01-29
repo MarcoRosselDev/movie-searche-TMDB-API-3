dffb7e9ab278cfd39b6ba382ef4eb07df5041ab3
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-01-29 15:48:12 -0300

    useNavigate film

--- src/pages/film.tsx
+++ src/pages/film.tsx
@@ -1,4 +1,6 @@
 import { useParams } from 'react-router-dom'
+import { Image } from '../components/image'
+import { Section } from '../components/section'
 import { MediaType } from '../types'
 
 interface Props {
@@ -9,5 +11,15 @@ export const Film = (props: Props) => {
   //
   const { params } = useParams()
 
-  return <div>film</div>
+  return (
+    <>
+      {/* background */}
+      <div className="h-[300px] absolute  left-0  right-0 top-0">
+        <Image src=""></Image>
+      </div>
+      <Section className="-mt-[150px] flex items-start relative z-10">
+        <Image src="" className="w-[92px] m-w-[92px] h-[192px]  "></Image>
+      </Section>
+    </>
+  )
 }
