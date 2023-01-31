5d1a1f0953f6a4b1475d5a3c4c2f0812ec420125
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-01-31 00:03:50 -0300

    catalog search 2

--- src/pages/catalog.tsx
+++ src/pages/catalog.tsx
@@ -1,5 +1,6 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useSearchParams } from 'react-router-dom'
+import { Card } from '../components/card'
 import { Image } from '../components/image'
 import { Section } from '../components/section'
 import { Film } from '../interfaces'
@@ -33,6 +34,21 @@ export const Catalog = (props: Props) => {
       break
   }
 
+  const fetch = () => {
+    const arrs: any[] = []
+
+    for (let i = 0; i < 20; i++) {
+      arrs.push({
+        title: 'lorems ipsum ...',
+      })
+    }
+    setFilms(arrs)
+  }
+
+  useEffect(() => {
+    fetch()
+  }, [])
+
   return (
     <>
       {/* background */}
@@ -45,7 +61,11 @@ export const Catalog = (props: Props) => {
         className="-mt-[150px] flex items-center relative z-10 mobile:block"
         title="PAGE TITLE"
       >
-        <div className="grid grid-cols-4"></div>
+        <div className="grid grid-cols-4">
+          {films.map((film, i) => (
+            <Card imageSrc="" title={film.title} key={i}></Card>
+          ))}
+        </div>
       </Section>
     </>
   )
