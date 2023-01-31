2eaa069479213e0f99bcd719b833b98699635220
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-01-30 23:16:10 -0300

    catalog background

--- src/pages/catalog.tsx
+++ src/pages/catalog.tsx
@@ -1,3 +1,7 @@
+import { useState } from 'react'
+import { Image } from '../components/image'
+import { Section } from '../components/section'
+import { Film } from '../interfaces'
 import { MediaType } from '../types'
 
 interface Props {
@@ -5,5 +9,20 @@ interface Props {
 }
 
 export const Catalog = (props: Props) => {
-  return <div>{props.type}</div>
+  //
+  const [films, setFilms] = useState<Film[]>([])
+  return (
+    <>
+      {/* background */}
+      <div className="h-[300px] left-0 right-0 top-0 relative">
+        <div className="overlay-film-cover"></div>
+        <Image src=""></Image>
+      </div>
+      {/* poster and text */}
+      <Section
+        className="-mt-[150px] flex items-center relative z-10 mobile:block"
+        title="PAGE TITLE"
+      ></Section>
+    </>
+  )
 }
