1b111fbd8b568c6b9f5e6ccd9d171c3045e114c7
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-02-02 01:29:36 -0300

    loading animation from react icons

--- src/components/app-container.tsx
+++ src/components/app-container.tsx
@@ -6,7 +6,8 @@ import { Body } from '../leyouts/Body'
 import { Footer } from '../leyouts/Footer'
 import { Genre } from '../interfaces'
 import { MediaType } from '../types'
-import { useContext, createContext } from 'react'
+import { useContext, createContext, useState } from 'react'
+import { Loading } from './loading'
 
 type Genres = {
   [key in MediaType]: Genre[]
@@ -18,12 +19,25 @@ const GlobalContext = createContext<{
   genres: {
     movie: [],
     tv: [],
-  } satisfies Genres,
+  },
 })
 
 export const useGlobalContext = () => useContext(GlobalContext)
 
 export const AppContainer = () => {
+  const [genres, setGenres] = useState<Genres>({
+    movie: [],
+    tv: [],
+  })
+
+  if (!genres.movie.length || !genres.movie.length) {
+    return (
+      <div className="fixed left-0 top-0 right-0 bottom-0 flex items-center justify-center">
+        <Loading></Loading>
+      </div>
+    )
+  }
+
   return (
     <BrowserRouter>
       <div className="pb-[64px]">
