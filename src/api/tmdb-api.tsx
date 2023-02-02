1ab748b5bf76278d2f8c5bb13c96c818dbb807fb
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-02-02 00:51:52 -0300

    search fixed successful

--- src/api/tmdb-api.tsx
+++ src/api/tmdb-api.tsx
@@ -11,6 +11,7 @@ axiosClient.interceptors.request.use((config) => {
   return {
     ...config,
     params: {
+      ...config.params,
       api_key: process.env.REACT_APP_TMDB_API_KEY,
     },
   }
