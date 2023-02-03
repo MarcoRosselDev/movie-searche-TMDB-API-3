815c909049a3591c4923916e2bafbcefd5afb7ed
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-02-03 06:31:54 -0300

    season detail

--- src/pages/season.tsx
+++ src/pages/season.tsx
@@ -1,51 +1,32 @@
 import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { getSeason } from '../api/tmdb-api'
 import { Image } from '../components/image'
 import { Section } from '../components/section'
-import { Film } from '../interfaces'
+import { Film, Season as SeasonInterface } from '../interfaces'
 
 export const Season = () => {
   //
-  const [film, setFilm] = useState<Film>({
-    id: 0,
-    coverPath: '',
-    title: 'random title',
-    description:
-      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi nemo repellendus quo corporis earum laudantium necessitatibus maxime culpa a! Aspernatur eos nam ex repudiandae provident, sed nulla fugit necessitatibus esse.',
-    genreIds: [1, 2, 3, 4],
-    mediaType: 'tv',
-    posterPath: '',
-    seasons: [
-      {
-        id: 1,
-        seasonNumber: 1,
-      },
-      {
-        id: 2,
-        seasonNumber: 2,
-      },
-      {
-        id: 3,
-        seasonNumber: 3,
-      },
-    ],
-  } as any)
+  const [season, setSeason] = useState<SeasonInterface | null>(null)
 
-  //
-  const [episodes, setEpisodes] = useState<any[]>()
-  //
-  const fetch = () => {
-    const arrs: any[] = []
-    for (let i = 0; i < 12; i++) {
-      arrs.push({})
-    }
+  const params = useParams<any>()
 
-    setEpisodes(arrs)
+  const fetch = async () => {
+    setSeason(
+      await getSeason(
+        parseInt(params.id as string),
+        parseInt(params.seasonNumber as string)
+      )
+    )
   }
-  //
+
   useEffect(() => {
     fetch()
   }, [])
 
+  if (!season) {
+    return <></>
+  }
   return (
     <>
       {/* background */}
@@ -60,15 +41,15 @@ export const Season = () => {
           className="w-[200px] min-w-[200px] h-[300px] mobile:mx-auto"
         ></Image>
         <div className="px-3 flex flex-col items-start gap-3">
-          <p className="text-xl line-clamp-1">{film.title}</p>
+          <p className="text-xl line-clamp-1">{season.filmName}</p>
           <p className="line-clamp-3 opacity-[0.9]">
-            Season 1 | {episodes?.length} episodes
+            Season 1 | {season.name} episodes
           </p>
         </div>
       </Section>
       {/* episodes */}
       <Section title="Episodes">
-        {episodes?.map((episodes, i) => (
+        {season.episodes.map((episodes, i) => (
           <div
             className="my-6 flex items-stretch gap-4 rounded-md overflow-hidden cursor-pointer hover:bg-primary px-3 py-1.5 mobile:block"
             key={i}
