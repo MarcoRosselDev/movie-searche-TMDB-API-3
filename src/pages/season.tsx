7342fb0ac1e51b1019ca9d304a4139689e9fa99c
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-02-03 06:49:28 -0300

    tmdb info seassons

--- src/pages/season.tsx
+++ src/pages/season.tsx
@@ -4,6 +4,7 @@ import { getSeason } from '../api/tmdb-api'
 import { Image } from '../components/image'
 import { Section } from '../components/section'
 import { Film, Season as SeasonInterface } from '../interfaces'
+import { tmdbImageSrc } from '../utils'
 
 export const Season = () => {
   //
@@ -32,12 +33,15 @@ export const Season = () => {
       {/* background */}
       <div className="h-[150px] left-0 right-0 top-0 relative">
         <div className="overlay-film-cover"></div>
-        <Image className="rounded-0 rounded-none" src=""></Image>
+        <Image
+          className="rounded-0 rounded-none"
+          src={tmdbImageSrc(season.posterPath)}
+        ></Image>
       </div>
       {/* poster and text */}
       <Section className="-mt-[75px] flex items-center relative z-10 mobile:block">
         <Image
-          src=""
+          src={tmdbImageSrc(season.posterPath)}
           className="w-[200px] min-w-[200px] h-[300px] mobile:mx-auto"
         ></Image>
         <div className="px-3 flex flex-col items-start gap-3">
@@ -54,20 +58,14 @@ export const Season = () => {
             className="my-6 flex items-stretch gap-4 rounded-md overflow-hidden cursor-pointer hover:bg-primary px-3 py-1.5 mobile:block"
             key={i}
           >
-            <Image src="" className="min-w-[300px] w-[300px] h-[150px]"></Image>
+            <Image
+              src={tmdbImageSrc(episodes.stillPath)}
+              className="min-w-[300px] w-[300px] h-[150px]"
+            ></Image>
             <div className="overflow-hidden flex flex-col gap-3 mobile:py-3">
-              <p className="text-lg truncate">
-                quo quasi? Quaerat, voluptas iusto. Consectetur deserunt omnis
-                accusamus in? Voluptatibus iste animi, eveniet perspiciatis
-                neque.
-              </p>
-              <p className="opacity-[0.9] line-clamp-5">
-                <div className="mt-auto pt-3 text-right">
-                  quo quasi? Quaerat, voluptas iusto. Consectetur deserunt omnis
-                  accusamus in? Voluptatibus iste animi, eveniet perspiciatis
-                  neque.
-                </div>
-              </p>
+              <p className="text-lg truncate">{episodes.title}</p>
+              <p className="opacity-[0.9] line-clamp-5">{episodes.overview}</p>
+              <div className="mt-auto pt-3 text-right"></div>
             </div>
           </div>
         ))}
