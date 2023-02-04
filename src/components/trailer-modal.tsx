7a206a75ba9443395fdc939230a89c6058133b45
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-02-04 04:53:32 -0300

    class name on trailer modal

--- src/components/trailer-modal.tsx
+++ src/components/trailer-modal.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react'
 import { Container } from './container'
 
+import { IoIosClose } from 'react-icons/io'
+
 interface Props {
   src: string | null
 }
@@ -14,24 +16,36 @@ export const TrailerModal = (props: Props) => {
     <div
       onClick={() => setShow(false)}
       className="
-      fixed 
-      z-[1080]
-      top-0 
-      bottom-0
-      left-0 
-      right-0 
-      after:fixed 
-      after:container-[''] 
-      after:h-full 
-      after:w-full 
-      after:bg-black
-      after:opacity-[0.9]
+            duration-300
+            fixed
+            z-[1080] 
+            top-0 
+            bottom-0 
+            left-0 
+            right-0
+            after:fixed
+            after:content-['']
+            after:top-0
+            after:bottom-0
+            after:left-0
+            after:right-0
+            after:bg-black
+            after:opacity-[0.9]
       "
     >
-      <Container className="bg-header rounded-sm my-12">
+      <Container
+        className="
+          relative 
+          z-10
+          transition-[margin,opacity]
+          ease-in-out
+          duration-300"
+      >
         <div onClick={(e) => e.stopPropagation()}>
           <div className="p-3 text-right">
-            <button onClick={() => setShow(false)}>{/* icons last */}</button>
+            <button onClick={() => setShow(false)}>
+              <IoIosClose size={18}></IoIosClose>
+            </button>
           </div>
           <iframe
             src={props.src as string}
